403fb366396b821276a51352553aadbc1db064b0
Nuo Mi <nuomi2021@gmail.com>
2023-05-31 17:09:12 +0800

    refact: create TestRunner to prepare for perf.py

--- tools/ffmpeg.py
+++ tools/ffmpeg.py
@@ -26,87 +26,18 @@ from sys import platform
 import re
 import concurrent.futures
 import subprocess
-
-
-def get_ref_md5(fn):
-    dir = dirname(fn)
-    name = basename(fn)
-    checksums_path = os.path.join(dir, "md5.txt")
-    try:
-        with open(checksums_path) as checksums_file:
-            checksums = checksums_file.readlines()
-        checksums = filter(lambda l: l.endswith("  " + name + "\n"), checksums)
-        checksum = next(checksums)
-        return checksum.split()[0]
-    except (FileNotFoundError, StopIteration):
-        return None
-
-
-def get_md5(input, ffmpeg_path):
-    cmd = ffmpeg_path + " -i " + input + " -vsync 0 -f md5 -"
-    print(cmd)
-    try:
-        o = subprocess.run(cmd.split(), capture_output=True, timeout=5 * 60)
-        if o.returncode:
-            print(o.stderr)
-            return ""
-        return o.stdout.decode().replace("MD5=", "").strip()
-    except Exception as e:
-        return str(e)
-
+from runner import *
 
 PASSED = 0
 FAILED = 1
 SKIPPED = 2
-
-
-def test(f, args):
-    refmd5 = get_ref_md5(f)
-    if not refmd5:
-        print(basename(f) + " has no ref md5")
-        return SKIPPED
-
-    md5 = get_md5(f, args.ffmpeg_path)
-    if refmd5 == md5:
-        return PASSED
-
-    print("md5 mismatch ref = " + refmd5 + " md5 = " + md5)
-    return FAILED
-
-
-def is_candidiate(f):
-    filename, ext = os.path.splitext(f)
-    ext = ext.lower()
-    supported = [".bin", ".bit", ".vvc", ".266"]
-    return ext in supported
-
-
-def check_input():
-    argc = len(sys.argv)
-    if argc != 2 and argc != 1:
-        print("usage: " + sys.argv[0] + " directory/file")
-        sys.exit(1)
-    return sys.argv[1]
-
-
-def test_file(path, args):
-    pss = test(path, args)
-    print(basename(path) + " passed" if pss == PASSED else " failed")
-    return 0
-
-
 def print_files(name, files):
     if len(files) > 0:
         print(name + " files:")
         for f in files:
             print("    " + f)
 
-
-count = [0, 0, 0]
-summary = [[], [], []]
-
-
-def print_summary():
+def print_summary(summary, count):
     print("")
     print("+++++++++ report +++++++++")
     print_files("failed", summary[FAILED])
@@ -125,57 +56,90 @@ def print_summary():
     )
     print("----------")
 
+def get_ref_md5(fn):
+    dir = dirname(fn)
+    name = basename(fn)
+    checksums_path = os.path.join(dir, "md5.txt")
+    try:
+        with open(checksums_path) as checksums_file:
+            checksums = checksums_file.readlines()
+        checksums = filter(lambda l: l.endswith("  " + name + "\n"), checksums)
+        checksum = next(checksums)
+        return checksum.split()[0]
+    except (FileNotFoundError, StopIteration):
+        return None
 
-def submmit_files(executor, path, args):
-    future_to_file = {}
-    for root, dirs, files in os.walk(path):
-        for f in files:
-            fn = join(root, f)
-            if is_candidiate(fn):
-                future_to_file[executor.submit(test, fn, args)] = f
-    return future_to_file
+class ConformanceRunner(TestRunner):
+    def run(self):
+        if os.path.isfile(self.args.test_path):
+            self.__test_file()
+        else:
+            self.__test_dir()
+
+    def add_args(self, parser):
+        parser.add_argument("-t", "--threads", type=int, default=4)
+
+    def __test_file(self):
+        pss = self.__test(self.args.test_path)
+        print(basename(self.args.test_path) + " passed" if pss == PASSED else " failed")
+        return 0
+
+    def __get_md5(self, input):
+        cmd = self.args.ffmpeg_path + " -i " + input + " -vsync 0 -f md5 -"
+        print(cmd)
+        try:
+            o = subprocess.run(cmd.split(), capture_output=True, timeout=5 * 60)
+            if o.returncode:
+                print(o.stderr)
+                return ""
+            return o.stdout.decode().replace("MD5=", "").strip()
+        except Exception as e:
+            return str(e)
+
+    def __test(self, f):
+        refmd5 = get_ref_md5(f)
+        if not refmd5:
+            print(basename(f) + " has no ref md5")
+            return SKIPPED
+        md5 = self.__get_md5(f)
+        if refmd5 == md5:
+            return PASSED
+
+        print("md5 mismatch ref = " + refmd5 + " md5 = " + md5)
+        return FAILED
 
 
-def test_dir(path, args):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
-        future_to_file = submmit_files(executor, path, args)
-        for future in concurrent.futures.as_completed(future_to_file):
-            f = future_to_file[future]
-            try:
-                s = future.result()
-            except Exception as e:
-                print("%s generated an exception: %s" % (f, e))
-            else:
-                count[s] += 1
-                summary[s].append(f)
+    def __submmit_files(self, executor, path):
+        future_to_file = {}
+        for root, dirs, files in os.walk(path):
+            for f in files:
+                fn = join(root, f)
+                if self.is_candidiate(fn):
+                    future_to_file[executor.submit(self.__test, fn)] = f
+        return future_to_file
 
-    print_summary()
-    sys.exit(count[FAILED])
+
+    def __test_dir(self):
+        summary = [[], [], []]
+        count = [0, 0, 0]
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.args.threads) as executor:
+            future_to_file = self.__submmit_files(executor, self.args.test_path)
+            for future in concurrent.futures.as_completed(future_to_file):
+                f = future_to_file[future]
+                try:
+                    s = future.result()
+                except Exception as e:
+                    print("%s generated an exception: %s" % (f, e))
+                else:
+                    count[s] += 1
+                    summary[s].append(f)
+
+        print_summary(summary, count)
+        sys.exit(count[FAILED])
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description="FFVVC test runner")
-
-    parser.add_argument("test_path", type=str)
-    parser.add_argument("-t", "--threads", type=int, default=4)
-    parser.add_argument(
-        "-f",
-        "--ffmpeg-path",
-        type=str,
-        default=(os.getenv("FFMPEG_PATH") if os.getenv("FFMPEG_PATH") else None),
-    )
-    args = parser.parse_args()
-
-    if args.ffmpeg_path is None:
-        print(
-            "No FFmpeg path provided. Please provide a path to an FFmpeg executable either with -f, --ffmpeg-path or the environment variable FFMPEG_PATH.",
-            file=sys.stderr,
-        )
-        exit(1)
-
-    if os.path.isfile(args.test_path):
-        test_file(args.test_path, args)
-    else:
-        test_dir(args.test_path, args)
+    t = ConformanceRunner()
+    t.check_input()
+    t.run()
---
+++ tools/runner.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
+import os
+
+class TestRunner:
+    def check_input(self):
+        parser = argparse.ArgumentParser(description="FFVVC test runner")
+
+        parser.add_argument("test_path", type=str)
+        parser.add_argument(
+            "-f",
+            "--ffmpeg-path",
+            type=str,
+            default=(os.getenv("FFMPEG_PATH") if os.getenv("FFMPEG_PATH") else None),
+        )
+
+        self.add_args(parser)
+
+        self.args = parser.parse_args()
+
+        if self.args.ffmpeg_path is None:
+            return Exception(
+                "No FFmpeg path provided. Please provide a path to an FFmpeg executable either with -f, --ffmpeg-path or the environment variable FFMPEG_PATH."
+            )
+        return None
+
+    def add_args(self, parser):
+        pass
+
+    @staticmethod
+    def is_candidiate(f):
+        filename, ext = os.path.splitext(f)
+        ext = ext.lower()
+        supported = [".bin", ".bit", ".vvc", ".266"]
+        return ext in supported
